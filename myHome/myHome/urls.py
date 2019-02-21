7563cb716eb2e12758e2562cc5fca43862dae46f
Restu Wahyu Kartiko <13516155@std.stei.itb.ac.id>
2019-02-21 20:52:07 +0700

    Add admin page and requirment.txt for deploy stuff

--- myHome/myHome/urls.py
+++ myHome/myHome/urls.py
@@ -6,4 +6,5 @@ from django.conf.urls import include
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('', include('core.urls')),
+    path("admin/", admin.site.urls),
 ]
